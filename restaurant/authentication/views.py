6125b2f5da945c7e1ff959c7e489818b6a72e287
Moustafa <assassin_2k@hotmail.com>
2017-03-18 23:09:44 -0400

    test

---
+++ restaurant/authentication/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
+#from .models import Profile
+from authentication.forms import RegistrationForm, UserForm
+# Create your views here.
+
+def index(request):
+	return render_to_response('index.html')
+
+
+
+
+@csrf_exempt
+def signUp(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(
+					username = form.cleaned_data['username'],
+					password = form.cleaned_data['password1'],
+					email = form.cleaned_data['email'],
+					first_name = form.cleaned_data['first_name'],
+					last_name = form.cleaned_data['last_name'])
+			return HttpResponseRedirect('/signup/successful')
+	else:
+		form = RegistrationForm()
+	variables = RequestContext(request, {'form': form})
+	return render_to_response('signup.html', variables)
+
+def signup_successful(request):
+	return render_to_response('signup_successful.html')
+
+@login_required
+def homepage(request):
+	user = UserProfile.objects.all()
+	context = {
+	'user':request.user,
+	
+	}
+	#return render_to_response('homepage.html')
+	return render_to_response('homepage.html', context)
+
+def logout_page(request):
+	logout(request)
+	return render_to_response('logout.html')
